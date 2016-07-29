ae3cba23e2e211a89a96ae6b545e0a91ce53de81
John Christian Lukas <clukas812@gmail.com>
2016-07-29 16:39:17 -0500

    add Query and BeautifulSoup imports, take metadata as a keyword argument with default value the metadata returned from Query.get_metadata (redcap default metadata), very metadata is proper format and all fields have mandatory information (field_name, field_type, select_choices_or_calculations), enter_redcap takes record_id as an argument, check return value of the redcap entry attempt and handle errors if upload was unsuccessful, add read_xml method for parsing pmc_result.xml and finding search articles (can use pmc or pmid as identifier, maybe others but havent explored yet)

--- objects/ArticleManager.py
+++ objects/ArticleManager.py
@@ -3,18 +3,44 @@
 
 from tkinter import *
 
-import sys, re
+import sys, re, os
+sys.path.append("{0}/Desktop/cbmi/reproduce/python/MedicalResearchTool/objects".format(os.environ['HOME']))
+sys.path.append("{0}/Desktop/cbmi/reproduce/python/MedicalResearchTool".format(os.environ['HOME']))
+
 import pycurl, io, json
 from pprint import pprint
+from bs4 import BeautifulSoup
+from Query import Query
 
 class ArticleManager(object):
 
-	def __init__(self):
+	def __init__(self,metadata=Query().get_metadata()):
 		self.indi = 0
+		self.verify_meta(metadata)
+		self.metadata = metadata
 
-	def enter_redcap(self,entry):
-		self.entry['record_id'] = '9b7057f5f8894c9c'
-		print("uploading")
+	def verify_meta(self,metadata):
+		example = """[{'select_choices_or_calculations': '', 'field_type': 'text', 'field_label': 'Record ID', 'field_name': 'record_id'},
+{'select_choices_or_calculations': '1, Hypothesis Driven | 2, Hypothesis Generating | 3, Unclear', 'field_type': 'radio', 'branching_logic': '', 'field_label': 'Is the research hypothesis-driven or hypothesis-generating?', 'field_name': 'hypothesis_gen_or_driv'}"""
+		if (type(metadata) is not list):
+			raise TypeError("metadata must be type 'list' but is type: {0} \n\nmetadata: {1} \nexample metadata: {2}".format(type(metadata),metadata,example))
+		for item in metadata:
+			if (type(item) is not dict):
+				raise TypeError("each list-item of metadata must be a dict but item: {} is type: {}\nexample metadata: {}".format(item,type(item),example))
+			try:
+				item['field_name']
+				item['field_type']
+				item['select_choices_or_calculations']
+			except KeyError as e:
+				raise KeyError("""each list-item in metadata must contain at least the keys: 1) 'field_name', 2) 'field_type', 3) 'select_choices_or_calculations' 
+									.\nitem: {}\nexample metadata: {}""".format(item,example))
+
+	#could do
+	#def enter_redcap(self,entry,**kwargs)
+	#so can only overwrite record_id if user provides record_id
+	def enter_redcap(self,entry,record_id=0):
+		#entry['record_id'] = record_id			#leave out for now so I dont destroy redcap...
+		entry['record_id'] = '9b7057f5f8894c9c'
 
 		from config import config
 		buf = io.BytesIO()
@@ -36,6 +62,18 @@ class ArticleManager(object):
 
 		self.redcap_return = buf.getvalue()
 		buf.close()
+		if (re.search(b'error',self.redcap_return)):
+			splitreturn = self.redcap_return.split(b'\\"')
+			fail_field = splitreturn[3].decode()
+			fail_reason = splitreturn[7].decode()
+			print("redcap entry failed on field: '{}'' \nbecause: '{}'".format(fail_field,fail_reason))
+			if (self.ask_question("Would you like to edit field: '{}'".format(fail_field))):
+				entry[fail_field] = self.ask("What is the value of {}?".format(fail_field),fail_field)
+				return self.enter_redcap(entry,record_id)
+			else:
+				print("retrying entry without that field")
+				del entry[fail_field]
+				return self.enter_redcap(entry,record_id)
 		return self.redcap_return
 
 	def ask(self,question,redcap):
@@ -121,8 +159,10 @@ class ArticleManager(object):
 	def assign(self,redcap,value):
 		if (redcap in self.entry):
 			self.entry[redcap] += "," + value
+			return self.entry[redcap]
 		else:
 			self.entry[redcap] = value
+			return value
 
 	def check(self,variable,value,display,info,redcap):
 		correct = self.check_boolean(variable,value,display,info,redcap)
@@ -154,3 +194,19 @@ class ArticleManager(object):
 			print("\n\n")
 			self.assign(redcap,value)
 			return 1
+
+	def read_xml(self,file,identifier,search_id):
+		with open(file,'r') as x:
+			bs = BeautifulSoup(x.read(),"lxml")
+
+		#TODO, make more efficient
+		for ass in bs.find_all("article"):
+			#if (ass.find('article-id',{'pub-id-type':'pmc'}).text == '3592787'):
+			if (ass.find('article-id',{'pub-id-type':identifier}).text == search_id):
+				if (ass.find(text=re.compile("The publisher of this article does not allow downloading of the full text in XML form"))):
+					#article isnt open access :(
+					return -1
+				#found open access article
+				return str(ass)
+		return -1		#article not found
+				
