baca081cf06e6accc42a67498c42c59e1165d161
John Christian Lukas <clukas812@gmail.com>
2016-08-04 02:10:52 -0500

    RawArticle no longer extends ArticleExtractor and takes a file as argument (rather than pubmed code) (primary purpose is to extract text from pdf), swich PMCArticle to XMLArticle and ClosedArticle to PDFArticle, XMLArticle and PDFArticle now take article_id and identifier as arguments (an example is: pmid and 26513432, leaves option for doi etc), metadata and run_style managed via keyword arguments and super calls, if search fails XMLArticle returns 0 rather than enter bs attribute

--- objects/Article.py
+++ objects/Article.py
@@ -8,27 +8,23 @@ from bs4 import BeautifulSoup
 import re,sys
 import textract, nltk
 
-text = "filler"
-
-class RawArticle(ArticleExtractor):
-	def __init__(self,pubmed):
-		self.pubmed_code = pubmed
-		self.text = self.get_text()
+class RawArticle(object):
+	def __init__(self,file):
+		self.text = self.get_text(file)
 
 
-	def get_text(self):		#TODO, clean up or remove proper nouns
-		pdf_file = "articles/" + self.pubmed_code + ".pdf"
-		
-		text = textract.process(pdf_file)
-		text = text.strip()
-		text = re.sub(b'\n+',b" ",text)
-		text = re.sub(b'\s+',b" ",text)
-		if (re.search(b'(.*)references',text,re.I)):
-			search = re.search(b'(.*?)\Wreferences',text,re.I)
-			if (search):
-				text = search.group(1)
-			else:
-				print("check reference")
+	def get_text(self,file):		#TODO, clean up or remove proper nouns
+		if (not re.search(r'.pdf',file)):
+			file = file + ".pdf"
+		try:
+			text = textract.process(file)
+			text = text.strip()
+			text = re.sub(b'\n+',b" ",text)
+			text = re.sub(b'\s+',b" ",text)
+			return text.decode("utf-8")
+		except Exception as e:
+			print("file: {} not found\ninformation from textract:\n\t{}".format(file,e))
+			return 0
 
 			#TODO
 		"""
@@ -46,35 +42,29 @@ class RawArticle(ArticleExtractor):
 		print text
 		sys.exit()
 		"""
-
-		return text.decode()
 		#self.sents = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s',text)
 
-class PMCArticle(ArticleExtractor,XMLExtractor):
+class XMLArticle(ArticleExtractor,XMLExtractor):
 
 	#TODO, fix here and others so that metadata has default if none provided
-	def __init__(self,pubmed,run_style,metadata):
-		self.pubmed_code = pubmed
+	def __init__(self,xmltext,run_style,article_id,identifier,**metadata):
 		self.indi = run_style
 		self.entry = {}
-		#self.extra = {}
-		super(PMCArticle,self).__init__(metadata)		#TODO, change elsewhere if this works
-
-		#self.fulltext = self.load_text().text 			#for pmc using online xml
-		self.fulltext = pubmed 			#change to 'text' or something similar if stick with xml format
+		if ('metadata' in metadata):
+			super(XMLArticle,self).__init__(run_style=run_style,metadata=metadata['metadata'])
+		else:
+			super(XMLArticle,self).__init__(run_style=run_style)
+		self.fulltext = xmltext
 		self.bs = BeautifulSoup(self.fulltext,"lxml")
+		self.article_id = article_id
+		self.identifier = identifier
 
-	def load_text(self):
-		ids = self.xml['PubmedArticle']['PubmedData']['ArticleIdList']['ArticleId']
-		for each_id in ids:
-			if (each_id['@IdType'] == "pmc"):
-				pmc = each_id['#text']
+	def pmid(self,bs):
 		try:
-			return requests.get("{0}pmc/articles/{1}/?report=reader".format(ncbi_site,pmc))
-		except UnboundLocalError as e:
-			print(e)
-			print("not a pmc article, maybe closed source?")
-			raise
+			return bs.find(("article-id",{"pub-id-type":"pmid"})).text
+		except AttributeError as e:
+			#field not found
+			return ""
 
 	def section(self,sections,tag):
 		tags = self.bs.find_all(tag)
@@ -91,12 +81,12 @@ class PMCArticle(ArticleExtractor,XMLExtractor):
 	def search(self,regex):
 		try:
 			return self.bs.find(text=regex).parent.parent
-		except IndexError:
-			#regex search returned no results
-			return self.bs
 		except AttributeError:
 			#match occurred at top level of tree
 			return self.bs
+		except IndexError:
+			#regex search returned no results
+			return 0
 
 	def xml_section(self,*titles):
 		body = self.bs.body
@@ -105,40 +95,11 @@ class PMCArticle(ArticleExtractor,XMLExtractor):
 				return body.find("sec",{"sec-type":title}).text
 			if (body.find("title",text=title)):
 				return body.find("title",text=re.compile(title,re.I)).parent.text
-		return body.text		#unable to find section
+		return self.bs.body		#unable to find section
 
-
-
-
-	
-	def download_pdf(self):
-		"""
-		if (not os.path.isdir("articles/")):
-			os.makedirs("articles/")
-
-		ids = self.xml['PubmedArticle']['PubmedData']['ArticleIdList']['ArticleId']
-		for each_id in ids:
-			if (each_id['@IdType'] == "pmc"):
-				pmc = each_id['#text']
-
-		xml = self.xml_load(ncbi_site + "pmc/" + pmc_tag + pmc)
-		links = xml['OA']['records']['record']['link']
-		for link in links:
-			print(link)
-			if (link['@format'] == 'pdf'):
-				href = link['@href']
-		print(href)
-		bashCommand = "wget " + href
-		subprocess.run(bashCommand.split())
-
-		bashCommand = "mv " + href.split('/')[-1] + " " + self.pubmed_code + ".pdf"
-		subprocess.run(bashCommand.split())
-		"""
-		return
-
+	#TODO, if xml_section doesnt find choice, use a search instead of entire body
 	def get_hypotheses(self):
 		return self._get_hypotheses(self.xml_section('background','introduction'))
-		return self._get_hypotheses(self.section(["background","introduction"],"h2").get_text())
 
 	def get_funding(self):		#nltk could improve; low priority now though
 		#self.bs.find_all(string=re.compile(r'fund[ie]'))
@@ -146,15 +107,13 @@ class PMCArticle(ArticleExtractor,XMLExtractor):
 
 	def get_inex_criteria(self):	#TODO, expand
 		return self._get_inex_criteria(self.xml_section('methods'))
-		return self._get_inex_criteria(self.section(["methods"],"h2").get_text())
 
 	def get_ontol_vocab(self): #TODO, if ontol occurs outside of inclusion / exclusion
 		return	#TODO
-		return self._get_ontol_vocab(text)
+		return self._get_ontol_vocab(self.xml_section('methods'))
 
 	def get_databases(self):
 		return self._get_databases(self.xml_section('methods'))
-		return self._get_databases(self.section(["methods"],"h2").get_text())
 
 	def get_query(self):
 		return #TODO
@@ -163,7 +122,6 @@ class PMCArticle(ArticleExtractor,XMLExtractor):
 
 	def get_nlp(self):
 		return self._get_nlp(self.xml_section('methods'))
-		return self._get_nlp(self.section(["methods"],"h2").get_text())
 
 	def get_analysis(self):
 		return #TODO
@@ -175,8 +133,6 @@ class PMCArticle(ArticleExtractor,XMLExtractor):
 
 	def get_limitations(self):
 		return self._get_limitations(self.xml_section('discussion','conclusion'))
-		return #TODO
-		return self._get_limiations(text)
 
 	def get_primary_research(self):
 		return #TODO
@@ -192,16 +148,18 @@ class PMCArticle(ArticleExtractor,XMLExtractor):
 		return #TODO
 		return self._get_institution(institution)
 
-class ClosedArticle(ArticleExtractor,XMLExtractor):
-
-	def __init__(self,pubmed,run_style,metadata):
-		self.pubmed_code = pubmed
+class PDFArticle(ArticleExtractor,XMLExtractor):
+	def __init__(self,file,run_style,article_id,identifier,**metadata):
 		self.indi = run_style
-		self.metadata = metadata
 		self.entry = {}
-		self.extra = {}
-		self.text = RawArticle(pubmed).get_text()
-	
+		if ('metadata' in metadata):
+			super(PDFArticle,self).__init__(run_style=run_style,metadata=metadata['metadata'])
+		else:
+			super(PDFArticle,self).__init__(run_style=run_style)
+		self.text = RawArticle(file).text
+		self.article_id = article_id
+		self.identifier = identifier
+
 	def get_clinical_domain_from_pdf(self):
 		for each_sent in nltk.sent_tokenize(self.text):
 			search = re.search(r'key.*?words(.*)',each_sent,re.I)
@@ -248,7 +206,6 @@ class ClosedArticle(ArticleExtractor,XMLExtractor):
 	def get_analysis(self):
 		return
 		return self._get_analysis(self.text)
-	# ^ same 	
 	def get_clear_analysis(self):
 		return
 		return self._get_clear_analysis(self.text)
@@ -256,26 +213,3 @@ class ClosedArticle(ArticleExtractor,XMLExtractor):
 
 	def get_institution(self,institution):
 		return self._get_institution(institution)
-
-	
-	
-"""
-	def get_databases():
-
-		def get_new_text(text):
-			search = re.search(r'Conclusion(.*)',text)
-			if (search):
-				return search.group(1)
-			else:
-				return text
-
-		new_text = get_new_text(self.text)
-
-		new_sents = nltk.tokenize.sent_tokenize(new_text)
-		found_methods = 0
-		for each_sent in new_sents:
-			if (re.search(r'Methods',each_sent) or re.search(r'METHODS',each_sent)):
-				found_methods = 1
-			if (found_methods == 1):
-				return
-"""
